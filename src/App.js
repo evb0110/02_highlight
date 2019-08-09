c3468c4c1f5b73c1e9932a63cb95085f9a8e0c34
evb0110 <7149553@gmail.com>
2019-08-09 09:36:17 +0300

    process

--- src/App.js
+++ src/App.js
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function New(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
     </div>
   );
 }
 
-export default App;
+function Popular(props) {
+  return (
+    <div className="wrap-item wrap-item-popular">
+      <span className="label">Popular!</span>
+      {props.children}
+    </div>
+  );
+}
+
+function Wrapper({ item }) {
+  if (item.views > 1000) return Popular;
+  if (item.views > 100) return New;
+  return a => a;
+}
+
+function Wrap(Inner) {
+  return function(props) {
+    const Outer = Wrapper(props);
+    return Wrapper({ ...props, children: Inner(props) });
+  };
+}
+
+const BetterArticle = Wrap(Article);
+
+function Article(props) {
+  return (
+    <div className="item item-article">
+      <h3>
+        <a href="#">{props.title}</a>
+      </h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  );
+}
+
+function Video(props) {
+  return (
+    <div className="item item-video">
+      <iframe
+        src={props.url}
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+      />
+      <p className="views">Просмотров: {props.views}</p>
+    </div>
+  );
+}
+
+function List(props) {
+  return props.list.map(item => {
+    switch (item.type) {
+      case 'video':
+        return <Video {...item} />;
+
+      case 'article':
+        return <Article {...item} />;
+    }
+  });
+}
+
+export default function App() {
+  const [list, setList] = useState([
+    {
+      type: 'video',
+      url:
+        'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 50,
+    },
+    {
+      type: 'video',
+      url:
+        'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 12,
+    },
+    {
+      type: 'article',
+      title: 'Невероятные события в неизвестном поселке...',
+      views: 175,
+    },
+    {
+      type: 'article',
+      title: 'Секретные данные были раскрыты!',
+      views: 1532,
+    },
+    {
+      type: 'video',
+      url:
+        'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 4253,
+    },
+    {
+      type: 'article',
+      title: 'Кот Бегемот обладает невероятной...',
+      views: 12,
+    },
+  ]);
+
+  return <List list={list} />;
+}
